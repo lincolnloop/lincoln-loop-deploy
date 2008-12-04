1175b70f7a4ee1ebbb124485b77ea28bfeec3395
Peter Baumgartner <pete@lincolnloop.com>
2008-12-04 08:59:52 -0700

    new repo list format

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,67 @@
+import os
+import sys
+        
+class Subversion(object):
+    """
+    Generates subversion command strings
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        for k,v in kwargs.items():
+            setattr(self, k, v)
+    def checkout(self):
+        cmd = 'svn '
+        if hasattr(self,'rev'):
+            cmd += '-r %s ' % self.rev
+        cmd += 'co %s ./src/%s' % (self.url, self.name)
+        return cmd
+
+def install_module(src_dir, module_name='', dist_utils=False):
+    """
+    Installs a Python module from the ./src directory either using
+    distutils or by symlinking the package to site-packages
+
+    """
+
+    #setup using distutils
+    if dist_utils:
+        cmd = '(cd src/%s;\\\n../../ve/bin/python setup.py install)' % src_dir
+    #symlink to site-packages
+    else:
+        src_path = os.path.join(src_dir,module_name)
+        cmd = '(cd ve/lib/python2.5/site-packages;\\\nln -s ../../../../src/%s .)' % (src_path)
+    return cmd
+        
+
 def bootstrap():
-    local("virtualenv ve")
-    local("mkdir src")
-    local("python grab_repos.py")
+    """
+    1. Creates a new virtualenv
+    2. Downloads all sources from fabreqs.py,
+       Adding them to the PYTHONPATH
     
+    """
+    #put the cwd on the path so we can use fabreqs.py
+    sys.path.append('.') 
+    from fabreqs import requirements
+    local('virtualenv ve')
+    #hack activate so it uses project directory instead of ve in prompt
+    local('sed \'s/(`basename \\\\"\\$VIRTUAL_ENV\\\\\"`)/(`basename \\\\`dirname \\\\"$VIRTUAL_ENV\\\\"\\\\``)/g\' ve/bin/activate > ve/bin/activate.tmp')
+    #sed 's/(`basename \\"\$VIRTUAL_ENV\\\"`)/(`basename \\`dirname \\"$VIRTUAL_ENV\\"\\``)/g'
+    local('mv ve/bin/activate.tmp ve/bin/activate')
+    local('mkdir src')
+    for pkg in requirements:
+        if pkg['dist'] == 'svn':
+            local(Subversion(**pkg).checkout())
+            #if a package name isn't specified, assume dist_utils
+            if pkg.has_key('package'):
+                local(install_module(pkg['name'], pkg['package']))
+            else:
+                local(install_module(pkg['name'], dist_utils=True))
+        elif pkg['dist'] == 'pypi':
+            cmd = './ve/bin/easy_install -a %s' % pkg['name']
+            if pkg.has_key('rev'):
+                cmd += '==%s' % pkg['rev']
+            local(cmd)
+
+                
---
+++ fabreqs.py
@@ -0,0 +1,39 @@
+requirements = [
+    {
+        'name': 'python-dateutil',
+        'dist': 'pypi',
+        'rev': '1.4.1',
+        'url':'http://pypi.python.org/pypi/dateutil',
+    }, {
+        'name': 'django-1.0',
+        'dist':'svn',
+        'url':'http://code.djangoproject.com/svn/django/branches/releases/1.0.X',
+    }, {
+        'name':'django-tagging',
+        'package':'tagging',
+        'dist':'svn',
+        'rev':'149',
+        'url':'http://django-tagging.googlecode.com/svn/trunk'
+    }, {
+        'name':'django-command-extensions',
+        'dist':'svn',
+        'rev':'135',
+        'url':' http://django-command-extensions.googlecode.com/svn/trunk',
+    }, {
+        'name':'django-registration',
+        'dist':'svn',
+        'rev':'168',
+        'url':'http://django-registration.googlecode.com/svn/trunk'
+    }, {
+        'name':'django-template-utils',
+        'dist':'svn',
+        'rev':'109',
+        'url':'http://django-template-utils.googlecode.com/svn/trunk',
+    }, {
+        'name':'sorl-thumbnail',
+        'package':'sorl',
+        'dist':'svn',
+        'rev':'430',
+        'url':'http://sorl-thumbnail.googlecode.com/svn/trunk',
+    }
+]
